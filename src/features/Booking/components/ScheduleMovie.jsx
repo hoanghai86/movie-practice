719beed7669b618589ed2917507e1bb2a838c30a
hoanghai86 <hainguyenlehoang86.gmail.com>
2022-12-22 17:26:07 +0700

    ''

--- src/features/Booking/components/ScheduleMovie.jsx
+++ src/features/Booking/components/ScheduleMovie.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Tabs } from "antd";
+import { Col, Row, Tabs } from "antd";
 import { getScheduleCinema } from "../redux/action";
 import { useState } from "react";
 import { useEffect } from "react";
@@ -11,13 +11,15 @@ const ScheduleMovie = () => {
 
   // khi vừa vào trang Home, thì hệ thống rạp cinemas không hiện ra, chỉ khi click sự kiện onChange thì nó mới hiện => ta muốn hiện rạp đầu tiên khi load trang Home thì làm như sau:
   // cinemas[0].maHeThongRap;  <= lấy mã của rạp đầu tiên
-  // dùng hàm useEffect để nó chạy lần đầu tiên, bỏ hàm getScheduleCinema vào useEffect, truyền mã rạp đầu tiên vào hàm get, sau khi hàm get nó chạy call API thì set state lại, ở đây ta có hàm setListSchedule
+  // dùng hàm useEffect để nó chạy lần đầu tiên, bỏ hàm getScheduleCinema vào useEffect, truyền mã rạp đầu tiên vào hàm get, sau khi hàm get nó chạy call API thì set state lại bằng hàm setListSchedule
 
   useEffect(() => {
     getScheduleCinema(cinemas[0]?.maHeThongRap).then((res) =>
       setListSchedule(res.data.content)
     );
   }, [cinemas]);
+
+  console.log(listSchedule);
 
   return (
     <div className="container mx-auto mt-10">
@@ -46,9 +48,27 @@ const ScheduleMovie = () => {
               //do backend trả về 1 mảng có 1 phần tử duy nhất nên ta [0] luôn
               listSchedule[0].lstCumRap.map((itemCumRap) => {
                 return (
-                  <p>
-                    {itemCumRap.tenCumRap} <br /> {itemCumRap.diaChi}
-                  </p>
+                  // {itemCumRap.tenCumRap} <br /> {itemCumRap.diaChi} <br/>
+                  <Row>
+                    <Col span={4}>
+                      {itemCumRap.tenCumRap} <br /> {itemCumRap.diaChi} <br />
+                      <p></p>
+                    </Col>
+                    <Col span={20}>
+                      {itemCumRap.danhSachPhim.map((itemDanhSachPhim) => {
+                        return (
+                          <Row>
+                            <Col span={4} >
+                              <img src={itemDanhSachPhim.hinhAnh} className="w-24"/>
+                            </Col>
+                            <Col span={20}>
+                              <h1>abs</h1>
+                            </Col>
+                          </Row>
+                        );
+                      })}
+                    </Col>
+                  </Row>
                 );
               }),
           };
