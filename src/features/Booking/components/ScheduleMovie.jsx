f82987c37dfee335d50639e7147150f61dd4764e
hoanghai86 <hainguyenlehoang86.gmail.com>
2022-12-24 11:27:07 +0700

    ''

--- src/features/Booking/components/ScheduleMovie.jsx
+++ src/features/Booking/components/ScheduleMovie.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Col, Row, Tabs } from "antd";
+import { Col, Divider, Row, Tabs, Tag } from "antd";
 import { getScheduleCinema } from "../redux/action";
 import { useState } from "react";
 import { useEffect } from "react";
+import moment from "moment/moment";
 
 const ScheduleMovie = () => {
   const [listSchedule, setListSchedule] = useState([]);
@@ -43,34 +44,90 @@ const ScheduleMovie = () => {
           return {
             label: <img className="w-24" src={itemRap.logo} />,
             key: itemRap.maHeThongRap,
-            children:
-              listSchedule.length > 0 && //kiểm tra chiều dài mảng trước khi map
-              //do backend trả về 1 mảng có 1 phần tử duy nhất nên ta [0] luôn
-              listSchedule[0].lstCumRap.map((itemCumRap) => {
-                return (
-                  // {itemCumRap.tenCumRap} <br /> {itemCumRap.diaChi} <br/>
-                  <Row>
-                    <Col span={4}>
-                      {itemCumRap.tenCumRap} <br /> {itemCumRap.diaChi} <br />
-                      <p></p>
-                    </Col>
-                    <Col span={20}>
-                      {itemCumRap.danhSachPhim.map((itemDanhSachPhim) => {
-                        return (
-                          <Row>
-                            <Col span={4} >
-                              <img src={itemDanhSachPhim.hinhAnh} className="w-24"/>
-                            </Col>
-                            <Col span={20}>
-                              <h1>abs</h1>
-                            </Col>
-                          </Row>
-                        );
-                      })}
-                    </Col>
-                  </Row>
-                );
-              }),
+            // children:
+            //   listSchedule.length > 0 && //kiểm tra chiều dài mảng trước khi map
+            //   //do backend trả về 1 mảng có 1 phần tử duy nhất nên ta [0] luôn
+            //   listSchedule[0].lstCumRap.map((itemCumRap) => {
+            //     // <p key={itemCumRap.maCumRap}>
+            //     //   {itemCumRap.tenCumRap} <br /> {itemCumRap.diaChi} <br />
+            //     // </p>)
+
+            //     return <Tabs tabPosition="left" />;
+            //   }),
+
+            children: (
+              <Tabs
+                tabPosition="left"
+                items={
+                  listSchedule.length > 0 &&
+                  listSchedule[0]?.lstCumRap.map((itemCumRap) => {
+                    return {
+                      label: (
+                        <p className="text-left w-1/2">
+                          {itemCumRap.tenCumRap} <br /> {itemCumRap.diaChi}
+                        </p>
+                      ),
+                      key: itemCumRap.maCumRap,
+                      children: itemCumRap.danhSachPhim.map(
+                        (itemDanhSachPhim) => {
+                          return (
+                            <div key={itemDanhSachPhim.maPhim}>
+                              <Row
+                                gutter={[5, 5]}
+                                key={itemDanhSachPhim.maPhim}
+                              >
+                                <Col span={6}>
+                                  <img
+                                    className="w-36 h-52 object-cover"
+                                    src={itemDanhSachPhim.hinhAnh}
+                                  />
+                                </Col>
+                                <Col span={18}>
+                                  <Row>{itemDanhSachPhim.tenPhim}</Row>
+                                  <Row>
+                                    {itemDanhSachPhim.lstLichChieuTheoPhim.map(
+                                      (itemLichChieuTheoPhim) => {
+                                        return (
+                                          <Row
+                                            gutter={[16, 24]}
+                                            key={
+                                              itemLichChieuTheoPhim.maLichChieu
+                                            }
+                                          >
+                                            <Col
+                                              className="gutter-row"
+                                              span={6}
+                                            >
+                                              <div>
+                                                <Tag
+                                                  color="magenta"
+                                                  className="mb-2"
+                                                >
+                                                  {moment(
+                                                    itemLichChieuTheoPhim.ngayChieuGioChieu
+                                                  ).format("DD/MM/yyyy hh:mm")}
+                                                </Tag>
+                                              </div>
+                                            </Col>
+                                          </Row>
+                                        );
+                                      }
+                                    )}
+                                  </Row>
+                                </Col>
+                                <p></p>
+                              </Row>
+                              <Divider orientation="left"></Divider>{" "}
+                              {/* kẻ đường viền ngăn cách */}
+                            </div>
+                          );
+                        }
+                      ),
+                    };
+                  })
+                }
+              />
+            ),
           };
         })}
       />
