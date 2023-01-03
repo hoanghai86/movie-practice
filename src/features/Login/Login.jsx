a3481ee1a0d40e742b80cc937b07f5bf6952f8b0
hoanghai86 <hainguyenlehoang86.gmail.com>
2023-01-03 17:20:56 +0700

    ''

--- src/features/Login/Login.jsx
+++ src/features/Login/Login.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import { Button, Form, Input } from "antd";
 
 const Login = () => {
-  return (
-    <div><h1>Login</h1></div>
-  )
-}
+  const onFinish = (values) => {
+    console.log(values);
+  };
 
-export default Login
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <Form name="basic" onFinish={onFinish}>
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default Login;
